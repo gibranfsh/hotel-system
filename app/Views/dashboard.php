d99dce3976b83550eed6184a393d39c961935e0c
Ilmagita <ilmagita@gmail.com>
2023-12-14 17:48:22 +0700

    Installed bootstrap

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,4 +1,13 @@
-<h1>Dashboard</h1><br></br>
-<a href='/reservations'>Reservations</a>
-<a href='/rooms'>Rooms</a>
-<a href='/reports'>Reports</a>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+        <a href='/reservations'>Reservations</a>
+    </div>
+    <div class="col">
+        <a href='/rooms'>Rooms</a>
+    </div>
+    <div class="col">
+        <a href='/reports'>Reports</a>
+    </div>
+  </div>
+</div>
