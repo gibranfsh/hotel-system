920208e1954666b9889be9b1a6a30adbfe0afcda
Ilmagita <ilmagita@gmail.com>
2023-12-15 18:55:04 +0700

    Done reservation frontend

--- app/Controllers/Reports.php
+++ app/Controllers/Reports.php
@@ -4,6 +4,43 @@ namespace App\Controllers;
 
 class Reports extends BaseController {
     public function index() : string {
-        return view('layout/header').view('layout/navbar').view('pages/reports').view('layout/footer');
+        $data = [
+            [
+                'month' => 1,       // nanti diekstrak dari checkout date aja
+                'year' => 2024,        // nanti diekstrak dari checkout date aja
+                'reservationID' => 1,
+                'billTotal' => 300
+            ],
+            [
+                'month' => 1,       // nanti diekstrak dari checkout date aja
+                'year' => 2024,        // nanti diekstrak dari checkout date aja
+                'reservationID' => 2,
+                'billTotal' => 400
+            ],
+            [
+                'month' => 1,       // nanti diekstrak dari checkout date aja
+                'year' => 2024,        // nanti diekstrak dari checkout date aja
+                'reservationID' => 3,
+                'billTotal' => 500
+            ],
+            [
+                'month' => 2,       // nanti diekstrak dari checkout date aja
+                'year' => 2024,        // nanti diekstrak dari checkout date aja
+                'reservationID' => 4,
+                'billTotal' => 100
+            ],
+            [
+                'month' => 3,
+                'year' => 2024,
+                'reservationID' => 5,
+                'billTotal' => 900
+            ]
+        ];
+
+        $viewData = [
+            'data' => $data,
+        ];
+
+        return view('layout/header').view('layout/navbar').view('pages/reports', $viewData).view('layout/footer');
     }
 }
