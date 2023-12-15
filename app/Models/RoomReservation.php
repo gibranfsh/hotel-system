d5820ed656b77aef3cc1c7c75c4a90148820d0ff
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2023-12-15 09:57:46 +0700

    feat: Models creation

---
+++ app/Models/RoomReservation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class RoomReservation extends Model
+{
+    protected $table            = 'roomreservations';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'reservationID',
+        'roomNumber',
+        'price',
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+}
