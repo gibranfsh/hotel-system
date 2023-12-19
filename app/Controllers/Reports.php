f0468d30bac51ea268222e81c934b8bc09c900a1
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2023-12-19 21:06:05 +0700

    fix: graph by checkOutDate

--- app/Controllers/Reports.php
+++ app/Controllers/Reports.php
@@ -3,22 +3,25 @@
 namespace App\Controllers;
 
 use App\Models\PaymentModel;
+use App\Models\ReservationModel;
 
 class Reports extends BaseController
 {
     public function index(): string
     {
         $paymentModel = new PaymentModel();
+        $reservationModel = new ReservationModel();
         // find payments where paymentStatus is 'Paid'
         $payments = $paymentModel->where('paymentStatus', 'Paid')->findAll();
 
         $data = [];
 
         foreach ($payments as $payment) {
-            // Extract month and year from created_at
-            $createdDate = date_create($payment['created_at']);
-            $month = date_format($createdDate, 'm');
-            $year = date_format($createdDate, 'Y');
+            // Extract month and year from checkOutDate on each reservation with paymetID = $payment['id']
+            $reservation = $reservationModel->where('paymentID', $payment['id'])->first();
+            $checkOutDate = date_create($reservation['checkOutDate']);
+            $month = date_format($checkOutDate, 'm');
+            $year = date_format($checkOutDate, 'Y');
 
             $data[] = [
                 'month' => $month,
